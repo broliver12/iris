fda837a6ee9adf839b4ab0ff33b87c0b541f3f39
Oliver Straszynski <ostraszynski@konradgroup.com>
2019-04-15 14:53:50 -0400

    Small refactor, added base fragment class and layout

---
+++ app/src/main/java/com/strasz/color_value_calculator/fragment/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.strasz.color_value_calculator.fragment;
+
+import androidx.fragment.app.Fragment;
+
+public class BaseFragment extends Fragment {
+
+
+
+
+}
--- app/src/main/java/com/strasz/color_value_calculator/main/MainActivity.java
+++ app/src/main/java/com/strasz/color_value_calculator/main/MainActivity.java
@@ -1,7 +1,10 @@
-package com.strasz.color_value_calculator;
+package com.strasz.color_value_calculator.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.strasz.color_value_calculator.R;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 
 
 }

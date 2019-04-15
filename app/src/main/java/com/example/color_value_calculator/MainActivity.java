fba5de9f7c142a19dfd267d0315b4500d278c14b
Oliver Straszynski <ostraszynski@konradgroup.com>
2019-04-15 11:40:32 -0400

    Initial project creation

---
+++ app/src/main/java/com/example/color_value_calculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.color_value_calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

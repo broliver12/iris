eb510388f4fbe5944bb41f6c03b3bccda68e42c6
Oliver Straszynski <oliver.straszynski@loblaw.ca>
2021-12-29 15:57:55 -0500

    fix 1

--- app/src/main/java/com/strasz/iris/viewmodel/IColorListViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.strasz.iris.viewmodel
-
-import com.strasz.iris.database.ColorModel
-import io.reactivex.Observable
-
-interface IColorListViewModel {
-    fun getSavedColorList(): Observable<List<ColorModel>>
-    fun removeColor(color: ColorModel)
-}

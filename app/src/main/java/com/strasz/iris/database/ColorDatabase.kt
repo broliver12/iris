98bc107aa6a140b9ae80f74d2958e4fcf8d7fa81
Oliver Straszynski <oliver.straszynski@loblaw.ca>
2021-12-29 16:05:56 -0500

    Gitignore fix 1

--- app/src/main/java/com/strasz/iris/database/ColorDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.strasz.iris.database
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [ColorModel::class], version = 1, exportSchema = true)
-abstract class ColorDatabase : RoomDatabase() {
-    abstract fun colorModelDao(): ColorModelDao
-}
